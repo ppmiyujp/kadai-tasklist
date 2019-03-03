2902781d0f98131f1083cd1e58d8656ff748b70f
sho <hot298@hotmail.co.jp>
2019-03-03 13:40:37 +0000

    kadai login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,11 @@
 class TasksController < ApplicationController
+    before_action :require_user_logged_in, only: [:index, :show]
     before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :correct_user, only: [:show, :edit, :update, :destroy]
     
   def index
-    @tasks = Task.all.page(params[:page])
+    @tasks = current_user.tasks.page(params[:page])
+    # @tasks = Task.all.page(params[:page])
   end
 
   def show
@@ -13,7 +16,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に登録されました'
@@ -46,6 +49,12 @@ class TasksController < ApplicationController
   end
 
   private
+  
+  def correct_user
+    if @task.user != current_user
+      redirect_to root_url
+    end
+  end
 
   def set_task
     @task = Task.find(params[:id])
